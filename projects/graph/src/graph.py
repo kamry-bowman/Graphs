aa1550c2693f23b89e0c45a2aff26447fb4a3792
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-04 12:35:58 -0700

    implement depth and breadth first traversal

--- projects/graph/src/graph.py
+++ projects/graph/src/graph.py
@@ -1,3 +1,5 @@
+from collections import deque
+from collections import defaultdict
 """
 Simple graph implementation
 """
@@ -5,5 +7,41 @@ Simple graph implementation
 
 class Graph:
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
-        pass  # TODO
+        self.vertices = set()
+        self.edges = defaultdict(set)
+
+    def add_vertex(self, vertex):
+        self.vertices.add(vertex)
+
+    def add_directed_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.edges[v1].add(v2)
+        else:
+            raise Exception(
+                'Both vertices must be created to use them in an edge')
+
+    def bft(self, start):
+        visited = set()
+        q = deque()
+        q.appendleft(start)
+        while q:
+            current = q.pop()
+            if current not in visited:
+                visited.add(current)
+                print(current)
+                for vertex in self.edges[current]:
+                    q.appendleft(vertex)
+
+    def dft(self, start):
+        visited = set()
+        s = []
+        s.append(start)
+        while s:
+            current = s.pop()
+            if current not in visited:
+                visited.add(current)
+                print(current)
+                for vertex in self.edges[current]:
+                    s.append(vertex)
