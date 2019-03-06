960a4081173a4e85ac89fc307366041b6f999375
Kamry Bowman <kamry.bowman@gmail.com>
2019-03-06 15:00:09 -0700

    faster approach via random attempts

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -22,8 +22,6 @@ roomGraph={494: [(1, 8), {'e': 457}], 492: [(1, 20), {'e': 400}], 493: [(2, 5), 
 world.loadGraph(roomGraph)
 player = Player("Name", world.startingRoom)
 
-graph = {}
-
 
 def find_route(player):
     rev_dir = {
@@ -32,6 +30,8 @@ def find_route(player):
         'e': 'w',
         'w': 'e'
     }
+
+    graph = {}
 
     steps = []
 
@@ -106,7 +106,12 @@ def find_route(player):
         room = player.currentRoom
 
         # loop over ? directions in current room we're in, if there is a ?, go there
-        for dir, id in graph[room.id].items():
+        options = [option for option in graph[room.id].items()]
+        random.randrange(4)
+        while options:
+            index = random.randrange(len(options))
+            dir, id = options[index]
+            options.pop(index)
             if id == '?':
                 move(dir)
                 break
@@ -123,14 +128,30 @@ def find_route(player):
 
     return steps
 
+def find_min_route(tries):
+    player = Player("Name", world.startingRoom)
+    best_so_far = find_route(player)
+    attempt = 1
+    while attempt < tries:
+        player = Player("Name", world.startingRoom)
+        candidate = find_route(player)
+        if len(candidate) < len(best_so_far):
+            best_so_far = candidate
+        attempt += 1
+    return best_so_far
 
 world.printRooms()
 
 
 # FILL THIS IN
-traversalPath = find_route(player)
-print(traversalPath)
-# traversalPath = ['n', 's']
+# traversalPath = find_min_route(200)
+traversalPath = ['s', 'w', 'e', 'n', 'n', 's', 'w', 'w', 's', 'w', 's', 's', 'n', 'n', 'e', 'n', 'e', 'n', 'w', 'w', 's', 'n', 'w', 's', 's', 's', 's', 'w', 'w', 'e', 's', 'w', 'e', 'n', 'e', 's', 's', 'w', 's', 'w', 's', 'w', 's',
+'n', 'e', 'n', 'w', 'w', 'w', 'e', 's', 's', 's', 's', 's', 's', 'e', 'w', 'n', 'e', 'w', 'n', 'n', 'n', 'w', 'w', 'w', 'w', 'e', 'e', 'e', 's', 's', 'w', 'e', 's', 'w', 'w', 'e', 'e', 's', 'n', 'n', 'n', 'w', 'e', 'n', 'e', 'n', 'w', 'w', 'e', 'e', 'n', 'e', 'e', 'e', 'n', 'w', 'w', 'n', 's', 'w', 'n', 'w', 'n', 'n', 'w', 'n', 'w', 'e', 'e', 'n', 'w', 'w', 'e', 'n', 's', 'e', 'n', 's', 'e', 'e', 'e', 'n', 'w', 'w', 'e', 'e', 'e', 'n', 'e', 'e', 'e', 'n', 's', 'e', 'e', 'n', 'n', 'e', 'n', 'n', 'w', 'n', 'n', 'w', 'n', 'w', 'e', 'e', 'w', 's', 'e', 's', 's', 'e', 'n', 'n', 'n', 'e', 'w', 'n', 'e', 'n', 'n', 's', 's', 'w', 'n', 'n', 'n', 'e', 'n', 'w', 'w', 'e', 'e', 's', 'e', 'n', 'e', 'e', 'w', 'w', 's', 's', 's', 's', 'e', 'n', 'e', 'n', 'e', 'n', 'n', 's', 'e', 'e', 'w', 's', 'n', 'w', 's', 'w', 'n', 's', 's', 'w', 'n', 'n', 's', 's', 's', 's', 'e', 'e', 'w', 'n', 'e', 'n', 's', 'e', 'n', 's', 'e', 'w', 'w', 'w', 's', 'w', 's', 'e', 'w', 'w', 'w', 'w', 's', 'e', 'e', 'w', 'w', 'n', 'n', 'n', 'w', 'n', 'n', 's', 'w', 'n', 's', 'e', 's', 'w', 'w', 'n', 'w', 'n',
+'n', 'w', 'e', 's', 's', 'e', 'n', 'n', 'n', 's', 's', 's', 's', 'w', 'w', 'n', 's', 'w', 'n', 's', 'e', 'e', 's', 'w', 'e', 'n', 'e', 'e', 'e', 'e', 's', 's', 'e', 'e', 'n', 's', 'w', 'w', 's', 's', 's', 's', 's', 'n', 'w', 'n', 's', 'w', 'n', 'n', 'n', 'w', 'n', 's', 'w', 'n', 'w', 'e', 's', 'e', 'e', 's', 's', 'w', 'n', 's', 'e', 's', 'w', 'w', 'n', 'n', 'w', 'n', 's', 'e', 's', 's', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 's', 's', 's', 's', 'w', 'n', 'n', 'n', 'n', 'n', 'n', 's', 's', 'w', 'e', 's', 's', 's', 'w', 'n', 'n', 's', 's', 'w', 'w', 'w', 'w', 'e', 's', 'w', 'e', 'n', 'e', 'e', 'n', 'w', 'e', 'n', 'n', 's', 's', 's', 'e', 'e', 's', 'w', 'w', 'e', 'e', 'e', 's', 'e', 'n', 's', 'e', 'e', 'e', 'e', 's', 'w', 'e', 's', 'e', 's', 'e', 'n', 'e', 'n', 'e', 'n', 'e', 'n', 'n', 'n', 's', 'e', 'n', 'n', 'e', 'e', 'n', 's', 'e', 'e', 'w', 'n', 's', 'w', 'w', 'n', 's', 'w', 's', 's', 'w', 's', 's', 'w', 'n', 'n', 'n', 's', 's', 's', 's', 'w', 's', 'w', 'n', 'n', 'e', 'n', 'n', 's', 's', 'w', 'n', 'n', 's', 's', 's', 's', 's', 'e', 's', 'e', 's', 'e', 'e', 's', 's',
+'s', 's', 'e', 'e', 'w', 'w', 's', 'e', 'e', 'w', 'w', 'n', 'n', 'e', 'n', 's', 'e', 'w', 'w', 'n', 'n', 'e', 'e', 'e', 'w', 's', 'n', 'w', 'w', 'n', 'e', 'e', 'e', 'w', 'w', 'w', 'w', 'w', 's', 'e', 'w', 'n', 'n', 'w', 's', 's', 'n', 'n', 'n', 'e', 'e', 's', 'n', 'e', 'e', 'e', 'w', 'w', 's', 'e', 'e', 'e', 'n', 's', 'e', 'w', 'w', 'w', 'w', 'n', 'w', 'w', 'n', 'e', 'n', 'e', 'n', 'e', 'e', 'e', 'e', 'w', 'n', 'e', 'w', 's', 'w', 's', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 'n', 'w', 's', 'n', 'w', 'n', 'e', 'n', 'n', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 's', 's', 'e', 'n', 'e', 'w', 's', 'w', 'w', 's', 's', 'w', 's', 'e', 'e', 'e', 'e', 'e', 's', 'n', 'w', 'w', 'w', 'w', 'w', 'w', 's', 'w', 'w', 'w', 's', 's', 'e', 's', 'n', 'w', 's', 'w', 's', 's', 's', 's', 's', 's', 's', 's', 's', 's', 'n', 'n', 'n', 'n', 'n', 'w', 'w', 'w', 'n', 's', 'e', 's', 'e', 's', 's', 's', 'n', 'n', 'n', 'w', 's', 'n', 'w', 's', 'n', 'e', 'n', 'e', 'n', 'w', 'e', 'n', 'w', 'e', 'n', 'n', 'w', 'e', 'n', 'e', 'w', 'w', 'e', 's', 's', 'w', 'w', 's', 'n', 'e', 'e', 'n', 'n', 'e', 'e',
+'s', 'e', 'e', 's', 'e', 'n', 'e', 's', 's', 'n', 'n', 'w', 's', 'w', 's', 'e', 's', 'e', 'w', 's', 'e', 'e', 'e', 'e', 'w', 'w', 'w', 'w', 'n', 'n', 'w', 's', 's', 's', 'e', 'w', 'n', 'n', 'n', 'n', 'n', 'w', 's', 's', 'n', 'n', 'w', 's', 's', 's', 's', 'n', 'e', 's', 's', 'w', 's', 's', 'n', 'n', 'e', 's', 's', 's', 's', 's', 'n', 'n', 'w', 's', 'n', 'e', 'n', 'n', 'e', 's', 's', 's', 'n', 'n', 'e', 's', 's', 'n', 'n', 'e', 's', 's', 'n', 'n', 'w', 'w', 'n', 'e', 'e', 'n', 'e', 'e', 's', 's', 's', 's', 'n', 'n', 'e', 'w', 'n', 'n', 'e', 'w', 'w', 's', 's', 'n', 'n', 'w', 's', 'w', 'w', 'w', 'n', 'n', 'n', 'w', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'e', 'e', 'w', 'n', 'n', 'w', 'w', 'w', 'w', 'n', 's', 'w', 'n', 's', 'w', 'n', 'w', 'w', 'e', 'n', 'w', 'e', 's', 'e', 's', 'w', 'w', 'w', 'w', 'e', 'n', 'n', 's', 'w', 'n', 'n', 's', 'w', 'w', 'w', 'e', 'e', 'e', 's', 'w', 'w', 'e', 's', 'n', 'e', 'e', 's', 'e', 'e', 'e', 's', 's', 'n', 'w', 's', 'w', 'w', 'w', 's', 'w', 's', 'e', 's', 's', 'w', 'n', 'w', 'e', 's', 'e', 'e', 's', 'w', 'w', 's', 'w', 's', 'n', 'e', 'n',
+'e', 'e', 'e', 'e', 'e', 'e', 's', 's', 'w', 's', 'w', 's', 's', 'w', 'e', 'n', 'w', 'e', 'n', 'e', 's', 's', 'n', 'n', 'n', 'e', 's', 's', 's', 's', 'w', 'e', 'n', 'e', 's', 'n', 'e', 's', 's', 's', 'w', 'e', 'n', 'e', 'w', 'n', 'n', 'w', 'w', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'w', 'n', 'w', 'w', 'w', 'e', 'e', 'e', 's', 'w', 'w', 's', 'w', 'n']
+
 visited_rooms = set()
 player.currentRoom = world.startingRoom
 visited_rooms.add(player.currentRoom)
